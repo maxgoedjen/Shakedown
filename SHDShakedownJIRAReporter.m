7c22f234ef0241251b857aa62af121e6a842bfb2
Max Goedjen <max.goedjen@gmail.com>
2013-05-01 01:44:02 -0700

    Basic issue creation (no attachments yet)

--- SHDShakedownJIRAReporter.m
+++ SHDShakedownJIRAReporter.m
@@ -25,16 +25,23 @@
     NSString *authString = [NSString stringWithFormat:@"%@:%@", self.login, self.password];
     NSString *basic = [NSString stringWithFormat:@"Basic %@", [self base64StringFromString:authString]];
     [create setValue:basic forHTTPHeaderField:@"Authorization"];
+    [create setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
     [create setHTTPMethod:@"POST"];
     
     NSDictionary *issueDict = @{
-                                @"project": @{@"key": self.project},
-                                @"summary": [NSString stringWithFormat:@"%@", bugReport.title],
-                                @"description": bugReport.formattedReport
+                                @"fields": @{
+                                    @"project": @{@"key": self.project},
+                                    @"summary": [NSString stringWithFormat:@"%@", bugReport.title],
+                                    @"description": bugReport.formattedReport,
+                                    @"assignee": @{@"name": self.login},
+                                    @"issuetype": @{@"name": @"Bug"},
+                                    @"reporter": @{@"name": self.login}
+                                }
                                 };
     
     [create setHTTPBody:[NSJSONSerialization dataWithJSONObject:issueDict options:0 error:nil]];
-    
+    NSLog(@"%@", [[NSString alloc] initWithData:create.HTTPBody encoding:NSUTF8StringEncoding]);
+
     [NSURLConnection sendAsynchronousRequest:create queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
         if (error) {
             [self.delegate shakedownFailedToFileBug:@"Unable to log in"];
