acb3c647d3ffa60a5a07780c4ad5a01ced888eda
Max Goedjen <max.goedjen@gmail.com>
2013-05-27 04:02:26 -0700

    Sorting, reorganization

---
+++ Example Project/ShakedownExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ShakedownExample
+//
+//  Created by Max Goedjen on 5/27/13.
+//  Copyright (c) 2013 Max Goedjen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Example Project/ShakedownExample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ShakedownExample
+//
+//  Created by Max Goedjen on 5/27/13.
+//  Copyright (c) 2013 Max Goedjen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
