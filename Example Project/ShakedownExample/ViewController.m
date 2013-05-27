172716027abe3b331145f6957db12c984d7bc050
Max Goedjen <max.goedjen@gmail.com>
2013-05-27 04:06:21 -0700

    Adding sample project

--- Example Project/ShakedownExample/ViewController.m
+++ Example Project/ShakedownExample/ViewController.m
@@ -7,23 +7,15 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
-@end
+#import "SHDShakedown.h"
+#import "SHDShakedownEmailReporter.h"
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidAppear:(BOOL)animated {
+    SHDShakedownEmailReporter *emailReporter = [[SHDShakedownEmailReporter alloc] init];
+    emailReporter.recipient = @"hello@test.com";
+    [[SHDShakedown sharedShakedown] setReporter:emailReporter];
 }
 
 @end
