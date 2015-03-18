2a9fa682d468849e0cea267c9608796fb8196b3a
Max Goedjen <max.goedjen@gmail.com>
2015-03-17 23:26:52 -0700

    Sample show

--- ShakedownSample/ViewController.swift
+++ ShakedownSample/ViewController.swift
@@ -10,18 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        let vc = ShakedownViewController()
-        
-        // Do any additional setup after loading the view, typically from a nib.
+    override func viewDidAppear(animated: Bool) {
+        Shakedown.reporter = nil
+        Shakedown.displayFromViewController(self)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
