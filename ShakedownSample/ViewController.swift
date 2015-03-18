4437a6bb5e8f37a0e715359aafe8718f53041370
Max Goedjen <max.goedjen@gmail.com>
2015-03-17 21:54:01 -0700

    Starting work on UI

--- ShakedownSample/ViewController.swift
+++ ShakedownSample/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let vc = ShakedownViewController()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
