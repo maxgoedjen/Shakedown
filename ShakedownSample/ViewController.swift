35c71dda2e3e17f55108d3885ea536e8c804ae0f
Max Goedjen <max.goedjen@gmail.com>
2015-03-17 19:33:09 -0700

    Tabula rasa

---
+++ ShakedownSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShakedownSample
+//
+//  Created by Max Goedjen on 3/17/15.
+//  Copyright (c) 2015 Max Goedjen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
