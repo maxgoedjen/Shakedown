1956b8101d40da171659c295ee0c7ea15dda52c8
Max Goedjen <max.goedjen@gmail.com>
2015-03-17 22:32:25 -0700

    Remove dead initializer

--- Shakedown/Models/BugReport.swift
+++ Shakedown/Models/BugReport.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 struct BugReport {
-    
     let title: String
     let description: String
     let reproducability: String
@@ -17,9 +16,4 @@ struct BugReport {
     let screenshot: UIImage
     let deviceConfiguration: [String : String]
     let deviceLog: String
-    
-    static func emptyReportWithScreenshot(screenshot: UIImage) -> BugReport {
-        return BugReport(title: "", description: "", reproducability: "", reproductionSteps: [], screenshot: UIImage(), deviceConfiguration: <#[String : String]#>, deviceLog: <#String#>)
-    }
-    
 }
