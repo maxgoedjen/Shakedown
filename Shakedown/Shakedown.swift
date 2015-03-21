73b11cbff024296dcc99096e2d0b67ea539327dd
Max Goedjen <max.goedjen@gmail.com>
2015-03-21 03:32:45 -0700

    Docs for public api for Shakedown

--- Shakedown/Shakedown.swift
+++ Shakedown/Shakedown.swift
@@ -10,6 +10,11 @@ import UIKit
 
 @objc class Shakedown {
     
+    /**
+    Show Shakedown programatically from a specific view controller
+    
+    :param: viewController View controller to present from
+    */
     class func displayFromViewController(viewController: UIViewController) {
         // Explicitly specify bundle for CocoaPods 0.35/0.36 packaging differences
         let storyboard = UIStoryboard(name: "Shakedown", bundle: NSBundle(forClass: ShakedownViewController.self))
@@ -17,6 +22,10 @@ import UIKit
         viewController.presentViewController(navController, animated: true, completion: nil)
     }
     
+    /**
+    Begin listening for "Shake" trigger. If the user shakes their device, Shakedown will show from the front-most view controller.
+    You may want to use `beginListeningForFiveFingerHold` if your app uses shaking to trigger another event (like undo)
+    */
     class func beginListeningForShakes() {
         let vc = ShakeTriggerViewController(nibName: nil, bundle: nil) {
             self.displayFromFrontViewController()
@@ -26,6 +35,10 @@ import UIKit
         root?.view.addSubview(vc.view)
     }
     
+    /**
+    Begin listening for "5 finger tap and hold" trigger. If user places 5 fingers on the screen and holds it for a second, Shakedown will show from the front-most view controller.
+    This is an appropriate trigger to use if you use shake to undo in your app.
+    */
     class func beginListeningForFiveFingerHold() {
         let root = UIApplication.sharedApplication().keyWindow?.rootViewController
         let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: "fiveFingerHold:")
