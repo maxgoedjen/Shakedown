bace570163ac480da2c74971b38a885e3f87190d
Max Goedjen <max.goedjen@gmail.com>
2015-03-26 01:05:17 -0700

    Add JSON stub for gist

--- ShakedownTests/File Uploaders/Log Uploaders/GistUploaderTests.swift
+++ ShakedownTests/File Uploaders/Log Uploaders/GistUploaderTests.swift
@@ -9,10 +9,23 @@
 import XCTest
 import Quick
 import Nimble
+import Mockingjay
 import ShakedownSample
 
 class GistUploaderTests: LogUploaderTests {
     
+    override var expectedURL: NSURL {
+        return NSURL(string: "http://i.imgur.com/HV2zEeU.png")!
+    }
+    
+    override func stubAndVerifyRequest(request: NSURLRequest) -> Response {
+        expect(request.URL.absoluteString) == "https://api.github.com/gists"
+        expect(request.valueForHTTPHeaderField("Authorization")) == "token TestToken"
+        expect(request.HTTPMethod) == "POST"
+        let response = NSHTTPURLResponse(URL: request.URL, statusCode: 201, HTTPVersion: nil, headerFields: nil)!
+        return .Success(response, json("Gist"))
+    }
+
     override var uploader: LogUploader {
         return GistUploader(authenticationToken: "TestToken")
     }
