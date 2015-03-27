f0dc9c02c65638abc88a2d232694cdc5199a0083
Max Goedjen <max.goedjen@gmail.com>
2015-03-26 23:55:29 -0700

    Pinning JIRA request/response

--- ShakedownTests/Reporters/JIRAReporterSpec.swift
+++ ShakedownTests/Reporters/JIRAReporterSpec.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import Nimble
+import ShakedownSample
+import Mockingjay
+
+class JIRAReporterSpec: ReporterSpec {
+    
+    override var reporter: Reporter {
+        return JIRAReporter(instanceURL: "https://maxgoedjen-shakedown.atlassian.net", username: "max", password: "password", projectKey: "ST", issueType: "Bug", reproducibilityField: "customfield_10027")
+    }
+    
+    override var expectedMessage: String {
+        return "#9"
+    }
+    
+    override func stubAndVerifyRequest(request: NSURLRequest) -> Response {
+        expect(request.URL.absoluteString) == "https://maxgoedjen-shakedown.atlassian.net/rest/api/2/issue/"
+        expect(request.valueForHTTPHeaderField("Authorization")) == "token TestToken"
+        let data = request.HTTPBody ?? request.HTTPBodyStream?.synchronouslyRead()
+        let parsedJSON: AnyObject = NSJSONSerialization.JSONObjectWithData(data!, options: nil, error: nil)!
+        expect(parsedJSON.description) == jsonObject("JIRARequest").description
+        expect(request.HTTPMethod) == "POST"
+        let response = NSHTTPURLResponse(URL: request.URL, statusCode: 201, HTTPVersion: nil, headerFields: nil)!
+        return .Success(response, jsonData("JIRAResponse"))
+    }
+    
+}
