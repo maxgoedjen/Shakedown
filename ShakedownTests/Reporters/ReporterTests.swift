fd3c7151af35478803b8072b16003050edba6d4d
Max Goedjen <max.goedjen@gmail.com>
2015-03-26 19:52:33 -0700

    Add reporter tests

---
+++ ShakedownTests/Reporters/ReporterTests.swift
@@ -0,0 +1,21 @@
+//
+//  ReporterTests.swift
+//  ShakedownSample
+//
+//  Created by Max Goedjen on 3/26/15.
+//  Copyright (c) 2015 Max Goedjen. All rights reserved.
+//
+
+import XCTest
+import Quick
+import Nimble
+import Mockingjay
+import ShakedownSample
+
+class ReporterTests: QuickSpec {
+    
+    override func spec() {
+
+    }
+    
+}
