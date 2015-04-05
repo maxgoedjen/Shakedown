749ca93052b37f4965702a31cb12b64434f7ac0c
Max Goedjen <max.goedjen@gmail.com>
2015-04-04 23:00:04 -0700

    Add map view to sample app to provide good screenshot

--- ShakedownSample/ViewController.swift
+++ ShakedownSample/ViewController.swift
@@ -14,7 +14,9 @@ class ViewController: UIViewController {
 //        Shakedown.configuration.reporter = nil
         Shakedown.logMessage("Test")
         Shakedown.configuration.additionalMetadata = ["A" : "B"]
-        Shakedown.displayFrom(viewController: self)
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(1 * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) {
+            Shakedown.displayFrom(viewController: self)
+        }
     }
 
 }
