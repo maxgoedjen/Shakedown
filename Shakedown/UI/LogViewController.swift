99e82f80f45dc47cc8d41d5915da8f9f264a91d0
Max Goedjen <max.goedjen@ gmail.com>
2015-04-11 00:48:57 -0500

    Adding log view controller.

---
+++ Shakedown/UI/LogViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LogViewController.swift
+//  ShakedownSample
+//
+//  Created by Max Goedjen on 4/11/15.
+//  Copyright (c) 2015 Max Goedjen. All rights reserved.
+//
+
+import UIKit
+
+class LogViewController: UIViewController {
+    
+    @IBOutlet var textView: UITextView!
+    @IBOutlet var backgroundImageView: UIImageView!
+
+    var report: BugReport?
+    
+    
+}
