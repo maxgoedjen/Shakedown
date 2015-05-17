1e05afd79bcbcbe121122dd071bef13ac52f2ea4
Max Goedjen <max.goedjen@gmail.com>
2015-05-16 23:29:20 -0700

    Sample periodic notification

--- ShakedownSample/ViewController.swift
+++ ShakedownSample/ViewController.swift
@@ -12,11 +12,17 @@ class ViewController: UIViewController {
 
     override func viewDidAppear(animated: Bool) {
 //        Shakedown.configuration.reporter = nil
+        NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "logMessage:", userInfo: nil, repeats: true)
         Shakedown.logMessage("Test")
         Shakedown.configuration.additionalMetadata = ["Username" : "TestUser"]
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(1 * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) {
             Shakedown.displayFrom(viewController: self)
         }
+    }
+    
+    func logMessage(timer: NSTimer) {
+        let number = arc4random_uniform(1000)
+        Shakedown.logMessage("Test \(number)")
     }
 
 }
