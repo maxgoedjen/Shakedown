fe8134934b91666162cbe62f3b89c24ea085d2c2
Max Goedjen <max.goedjen@gmail.com>
2015-11-11 21:28:20 -0800

    One more guard over if let

--- Shakedown/Extensions/NSURLResponse+Error.swift
+++ Shakedown/Extensions/NSURLResponse+Error.swift
@@ -3,14 +3,12 @@ import Foundation
 extension NSURLResponse {
     
     var httpError: NSError? {
-        if let response = self as? NSHTTPURLResponse {
-            if 200..<300 ~= response.statusCode {
-                return nil
-            } else {
-                return NSError(domain: "NSURLHTTPResponseErrorDomain", code: response.statusCode, userInfo: nil)
-            }
+        guard let response = self as? NSHTTPURLResponse else { return nil }
+        if 200..<300 ~= response.statusCode {
+            return nil
+        } else {
+            return NSError(domain: "NSURLHTTPResponseErrorDomain", code: response.statusCode, userInfo: nil)
         }
-        return nil
     }
     
 }
