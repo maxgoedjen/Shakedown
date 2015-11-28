bd8864b65bfaa1d6bef4859b19dc93a444f0f5d9
Max Goedjen <max.goedjen@gmail.com>
2015-11-27 22:41:33 -0800

    Submitting animation

--- Shakedown/UI/Views/SubmittingView.swift
+++ Shakedown/UI/Views/SubmittingView.swift
@@ -10,16 +10,15 @@ import UIKit
 
 class SubmittingView: UIView {
     
-    let shapeLayer = CAShapeLayer()
-    let iPhonePath: UIBezierPath = {
-        let path = UIBezierPath()
-        path.moveToPoint(CGPoint(x: 10, y: 10))
-        path.addLineToPoint(CGPoint(x: 90, y: 10))
-        path.moveToPoint(CGPoint(x: 10, y: 20))
-        path.addLineToPoint(CGPoint(x: 90, y: 20))
-        path.moveToPoint(CGPoint(x: 10, y: 30))
-        path.addLineToPoint(CGPoint(x: 90, y: 30))
-        return path
+    
+    let backing = UIVisualEffectView(effect: UIBlurEffect(style: .Dark))
+    static let paths: [UIBezierPath] = {
+        return [UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: 100, height: 206), cornerRadius: 10),
+            UIBezierPath(roundedRect: CGRect(x: 2, y: 2, width: 96, height: 202), cornerRadius: 8),
+            UIBezierPath(roundedRect: CGRect(x: 42, y: 13, width: 16, height: 2), cornerRadius: 1),
+            UIBezierPath(ovalInRect: CGRect(x: 42, y: 185, width: 16, height: 16)),
+            UIBezierPath(ovalInRect: CGRect(x: 43, y: 186, width: 14, height: 14)),
+            UIBezierPath(rect: CGRect(x: 6, y: 25, width: 88, height: 157))]
     }()
     
     override init(frame: CGRect) {
@@ -32,29 +31,41 @@ class SubmittingView: UIView {
         setupView()
     }
     
+    let shapeLayers: [CAShapeLayer] = {
+        return paths.map { path in
+            let shapeLayer = CAShapeLayer()
+            shapeLayer.fillColor = UIColor.clearColor().CGColor
+            shapeLayer.strokeColor = UIColor.whiteColor().CGColor
+            shapeLayer.lineWidth = 1
+            shapeLayer.path = path.CGPath
+            shapeLayer.position = CGPoint(x: 137, y: 200)
+            return shapeLayer
+        }
+    }()
+    
     func setupView() {
-        backgroundColor = UIColor.clearColor()
-        let backing = UIVisualEffectView(effect: UIBlurEffect(style: .Dark))
+        backgroundColor = UIColor.blackColor()
         let views = ["v": backing]
         addSubview(backing)
         backing.translatesAutoresizingMaskIntoConstraints = false
         addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[v]|", options: [], metrics: nil, views: views))
         addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[v]|", options: [], metrics: nil, views: views))
+        for shapeLayer in shapeLayers {
+            layer.addSublayer(shapeLayer)
+        }
         
-        shapeLayer.strokeColor = UIColor.whiteColor().CGColor
-        shapeLayer.lineWidth = 1
-        shapeLayer.path = iPhonePath.CGPath
-        layer.addSublayer(shapeLayer)
-        shapeLayer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
     }
     
     func animate() {
-        shapeLayer.strokeEnd = 0
-        let animation = CABasicAnimation(keyPath: "strokeEnd")
-        animation.fromValue = 0
-        animation.toValue = 1
-        animation.duration = 10
-        shapeLayer.addAnimation(animation, forKey: "stroke")
+        for shapeLayer in shapeLayers {
+            shapeLayer.strokeEnd = 1
+            let animation = CABasicAnimation(keyPath: "strokeEnd")
+            animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+            animation.fromValue = 0
+            animation.toValue = 1
+            animation.duration = 3
+            shapeLayer.addAnimation(animation, forKey: "stroke")
+        }
     }
     
     override func didMoveToSuperview() {
